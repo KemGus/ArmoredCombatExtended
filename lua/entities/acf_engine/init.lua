321de3643a5fb51f7f4111b045c8baf223b68d39
RDC <basilscott@rocketmail.com>
2025-04-05 18:00:47 -0400

    Increased the distance engines can be heard from based on horsepower

--- lua/entities/acf_engine/init.lua
+++ lua/entities/acf_engine/init.lua
@@ -129,15 +129,22 @@ do
 		Engine.Heat             = ACE.AmbientTemp
 
 
+		local EngineHorsepower = Engine.peakkw / 0.7457 --Converts KW to HP, 74.57 / 100
+
 		Engine.TorqueScale	= ACE.TorqueScale[Engine.EngineType]
 
-		if Engine.peakkw > (74.57 / 100 * ACE.LargeEngineThreshold) and ACE.LargeEnginesRequireDrivers ~= 0 then --If the engine has more than 100 hp it requires a driver.
+		Engine.MaxDB = 50 + 60 * (EngineHorsepower / 2400) --Base volume of 70DB. Plus 60 * The ratio of the engine hp to 2400.
+
+		if EngineHorsepower > ACE.LargeEngineThreshold and ACE.LargeEnginesRequireDrivers ~= 0 then --If the engine has more than 100 hp it requires a driver.
 			Engine.RequiresDriver = true
 			Engine.CanUseSeatDriver = true
 		end
+
 		--calculate base fuel usage
 		if Engine.EngineType == "Electric" then
 			Engine.FuelUse = ACE.ElecRate / (ACE.Efficiency[Engine.EngineType] * 60 * 60) --elecs use current power output, not max
+
+			Engine.MaxDB = Engine.MaxDB * 0.15 --Electrics generate hardly any sound by themselves.
 		else
 			Engine.FuelUse = ACE.FuelRate * ACE.Efficiency[Engine.EngineType] * Engine.peakkw / (60 * 60)
 		end
@@ -395,8 +402,9 @@ function ENT:TriggerInput( iname, value )
 					filter:AddAllPlayers()
 
 					self.Sound = CreateSound(self, self.SoundPath , filter)
+					self.Sound:SetSoundLevel( self.MaxDB ) --Has to be adjusted before being played sadly. No dynamic DB levels.
 					self.Sound:PlayEx(0.5,100)
-
+					--print("Engine DB: " .. SoundStrength * self.MaxDB)
 				end
 				self:ACFInit()
 			else
@@ -794,7 +802,11 @@ function ENT:CalcRPM()
 
 	if self.Sound then
 		self.Sound:ChangePitch( math.min( 20 + (SmoothRPM * (self.SoundPitch / 100)) / 50, 255 ), 0 )
-		self.Sound:ChangeVolume( 0.25 + (0.1 + 0.9 * ((SmoothRPM / self.LimitRPM) ^ 1.5)) * self.Throttle / 1.5, 0 )
+		local SoundStrength = 0.25 + (0.1 + 0.9 * ((SmoothRPM / self.LimitRPM) ^ 1.5)) * self.Throttle / 1.5
+
+		self.Sound:ChangeVolume( SoundStrength, 0 )
+		--self.Sound:SetSoundLevel( SoundStrength * self.MaxDB, 0 ) --Will not work after being played.
+
 	end
 
 	return RPM
